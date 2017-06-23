0748bb1e61680a732c69a6cae26291c8ff06e8e1
Framky007 <ajapsfranklin@gmail.com>
2017-06-23 12:48:38 +0100

    refactor: redesign 404 component
    redesign the 404 component to conform to a uniform color scheme
    
    <finishes>

--- src/Pages/Page404.jsx
+++ src/Pages/Page404.jsx
@@ -12,16 +12,12 @@ class Page404 extends React.Component {
   render() {
     return (
       <div className="page404">
-        <Header />
         <h1 className="p404Header">PAGE NOT FOUND </h1>
-        <h3 className="p404Body"> You are reaching here because
-        the requested page cannot be found or it has been moved</h3>
-        <h4 className="p404Body">Click on the link below to go back to Home page
-        or click 'back' in the navigation bar to go to the previos page</h4>
+        <p className="p404Body"> You are reaching here because
+        the requested page cannot be found or it has been moved</p>
+        <p className="p404Body">Click on the link below to go back to Home page
+        or click 'back' in the navigation bar to go to the previos page</p>
         <p className="p404Link"><a href="/">Go to Home Page</a></p>
-
-        <Footer />
-
       </div>
     );
   }
